fed9b0359383a107d2e7ac5a4e0e3a2795901a7e
kyle-law <junwei_1121@hotmail.com>
2020-09-18 02:19:03 +0800

    Add: Add random background color and setInterval properties, also tags property for each quote object

---
+++ README.md
@@ -0,0 +1,37 @@
+# Portfolio Tracker
+
+> A Simple Random Quote Generator Web App.
+
+## Snapshots
+
+![image](https://user-images.githubusercontent.com/55923773/93202925-1ea48c00-f786-11ea-8b30-88b865301d19.png)
+
+## Presentations
+
+- View the live version [HERE](https://kyle-tracker.herokuapp.com/)
+
+## Built With
+
+- JavaScript
+
+## Authors
+
+👤 **Kyle Law**
+
+- Github: [@Kyle-Law](https://github.com/Kyle-Law)
+- Twitter: [@Kyle-Law](https://twitter.com/ZhunKhing)
+- Linkedin: [Kyle law](https://www.linkedin.com/in/kyle-lawzhunkhing/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/Kyle-Law/react-to-pokemon/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](LICENSE) licensed.
--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// For assistance:
-// Check the "Project Resources" section of the project instructions
-// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 /***
+ * A quotes array that store a list of quotes, each quote includes properties like quote, source(author), citation, year, and tags
  * `quotes` array
+ *
  ***/
 const quotes = [
   {
     quote:
       "The ability to work hard for days on end without losing focus is a talent. The ability to keep absorbing new information after many hours of study is a talent",
     source: "Garry Kasparov",
-    citation: "<How Life Imitates Chess",
+    citation: "How Life Imitates Chess",
     year: "2001",
+    tags: "chess",
   },
   {
     quote:
@@ -24,6 +18,7 @@ const quotes = [
     source: "Brian W. Kernighan and P. J. Plauger",
     citation: "The Elements of Programming Style.",
     year: "1978",
+    tags: "programming",
   },
   {
     quote:
@@ -31,6 +26,7 @@ const quotes = [
     source: "John Carmack",
     citation: "Graphics Programming Black Book",
     year: "1997",
+    tags: "programming",
   },
   {
     quote:
@@ -38,6 +34,7 @@ const quotes = [
     source: "Kernighan and Plauger",
     citation: "The Elements of Programming Style",
     year: "1978",
+    tags: "programming",
   },
   {
     quote:
@@ -45,11 +42,15 @@ const quotes = [
     source: "Jim McCarthy and Michele McCarthy",
     citation: "Software for your Head",
     year: "2002",
+    tags: "programming",
   },
 ];
 
 /***
+ * To generate a random quote Object
  * `getRandomQuote` function
+ * @params: none
+ * @return: {Object} A quote object within quotes array
  ***/
 function getRandomQuote() {
   // random number 0-4
@@ -58,7 +59,25 @@ function getRandomQuote() {
 }
 
 /***
+ * A  function to generate random background color of the DOM
+ * @params: none
+ * @return: none
+ *
+ * ***/
+function randomBodyColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const bgColor = `rgb(${r},${g},${b})`;
+
+  document.body.style.background = bgColor;
+}
+
+/***
+ * A function to display quote on the DOM
  * `printQuote` function
+ * @param: none
+ * @return: none
  ***/
 
 function printQuote() {
@@ -66,8 +85,12 @@ function printQuote() {
   document.querySelector(".quote").textContent = quoteObj.quote;
   document.querySelector(
     ".source"
-  ).innerHTML = `${quoteObj.source}<span class="citation">${quoteObj.citation}</span><span class="year">${quoteObj.year}</span>`;
+  ).innerHTML = `${quoteObj.source}<span class="citation">${quoteObj.citation}</span><span class="year">${quoteObj.year}</span><span class="tags"> ${quoteObj.tags}</span>`;
+  randomBodyColor();
 }
+
+// The quote is printed per 10 seconds
+setInterval(printQuote, 10000);
 
 /***
  * click event listener for the print quote button
